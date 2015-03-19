f9761ebe1699b24f1b35099552027002a824c6a2
松岡　直樹 <naokimatsuoka@naoki-no-MacBook-Pro.local>
2015-03-19 11:28:39 +0900

    spec4

--- src/Readable.java
+++ src/Readable.java
@@ -3,7 +3,9 @@ import data.Recipes;
 public class Readable {
 
     public static void main(String args[]) {
-        System.out.println(recipes.Recipes.EAT);
+        System.out.println(data.Recipes.omu);
+        System.out.println(data.Recipes.don);
+        System.out.println(data.Recipes.desert);
     }
 
 }
--- src/data/Recipes.java
+++ src/data/Recipes.java
@@ -2,5 +2,8 @@ package data;
 
 public class Recipes {
 
-    public static final String EAT = "オムライス";
+    public static final String omu = "オムライス";
+    public static final String don = "親子丼";
+    public static final String desert = "杏仁豆腐";
+
 }
